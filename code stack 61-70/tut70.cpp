a68995e24331a48c9c1bbf2a2aec59ef2e7753f9
jatin chourasiya <chaurasiyajatin68@gmail.com>
2023-12-26 17:41:48 +0530

    jaitn

--- code stack 61-70/tut70.cpp
+++ code stack 61-70/tut70.cpp
@@ -7,19 +7,34 @@ iterator iterates data (which points an element of a container)
 
 CONTAINERS
 it has three types:-
-1.sequence containers
+          1.sequence containers
 sequence containers stores data in linear fashion.
 examples: vectors,list,dequeue,
+          1.vector
+random excess fast
+in middile of stack insertion deletion slow(we have to work manually in it)
+insertion at the end = fast
+          2.list
+random excess slow
+insertion in middile fast
+insertion at the end is fast
 
-2.associative containers
+
+
+                2.associative containers
 direct access (map , multimap)(set, multiset)
 (fast searching,deletion,access)
 stores data in tree like structure
+insertion in middile and at the end of insertion and deletion fast
+random excess is slow
 
-3.derived containers
+
+
+                  3.derived containers
 they are derived from sequence containers and multi conationers
 real world modelling
 ex: stack, queue, priority queue
+depends (in data structure)
 
 
 
--- code stack 71-80/tut71.cpp
+++ code stack 71-80/tut71.cpp
@@ -2,33 +2,43 @@
 #include<vector>
 using namespace std;
 //vector manages its size automatically (we can add as many elements as we want)
-void diaplay(vector<int> &v)
+// vector methods
+template<class T>
+void display(vector<T> &v)
 {
+    cout<<"diaplaying this vector::"<<endl;
     for (int i = 0; i < v.size(); i++)
     {
         cout<<v[i]<<"   ";
+        // cout<<v.at(i)<<"   ";
     }
     cout<<endl;
     
 }
 int main(){
-    vector<int> vec1;
-    int element,size;
-    cout<<"enter the size of your vector:"<<endl;
-    cin>>size;
-    for (int i = 0; i < size; i++)
-    {
-        cout<<"enter an element to add to this vector:"<<endl;
-        cin>>element;
-        vec1.push_back(element);
+    //ways to creat a vector
+    vector<int> vec1;//zero length integer vector
+     vector<char> vec2(4);//4 element characher vector
+      vector<char> vec3(vec2);//4 element character vector from vec2
+       vector<int> v(6,3);//6 element vector of 3s
+       vec2.push_back('5');
+       display(v);
+    int element,size =5;
+    // cout<<"enter the size of your vector:"<<endl;
+    // cin>>size;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout<<"enter an element to add to this vector:"<<endl;
+    //     cin>>element;
+    //     vec1.push_back(element);
         
-    }
-    vec1.pop_back();//do not displays last element
-    vector<int> :: iterator iter = vec1.begin();
-    vec1.insert(iter+1,5, 566);
+    // }
+    // vec1.pop_back();//do not displays last element(removes last element)
+    // vector<int> :: iterator iter = vec1.begin();
+    // // vec1.insert(iter+1,5, 566);//adding extra element with its cloning which is also possible
 
    
-    diaplay(vec1);
+    // diaplay(vec1);
 
     
     return 0;
