4295d032fa8f51ecccfbb64e1b3887606e7233f7
jatin chourasiya <chaurasiyajatin68@gmail.com>
2024-01-04 00:03:43 +0530

    hvhg

--- code stack 41-50/tut46.cpp
+++ code stack 41-50/tut46.cpp
@@ -1,6 +1,25 @@
 /*
 CONSTRUCTORS IN DERIVED CLASS
 
+1.>we can use constructors in derived classes in c++
+2.>if base class constructor does not have any arguments , there is no need of any constructor
+   in derived class
+3.>but if there are one or more arguments in the base class constructor, derived class need to pass
+   arguments to the base class constructor 
+4.>if both base and derived classes have constructors , base class constructor is executed first.
+5.>in multiple inheritance ,base classes are constructed in the order in which they appear in the 
+   class decleration
+6.>in multi level inheritance , the constructor are executed in the order of inheritance   
+
+
+SPECIAL SYNTAX:-
+
+1.>c++ supports an special syntax for passing arguments to multiple base classes
+2.>the constructor of the derived class recieves all the arguments at once and then will pass
+   the calls to the respective base classes
+3.>the body is called after all the constructors are finished executing.
+
+
 */
 #include<iostream>
 using namespace std;
--- code stack 41-50/tut47.cpp
+++ code stack 41-50/tut47.cpp
@@ -2,9 +2,49 @@
 
 */
 #include<iostream>
+#include<cmath>
 using namespace std;
+class simplecalculator{
 
+int a,b;
+public:
+void getdata(){
+    cout<<"enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"enter the value of b:"<<endl;
+    cin>>b;
+}
+void performoperations(){
+cout<<"the value of a+b is :"<<a+b<<endl;
+cout<<"the value of a-b is :"<<a-b<<endl;
+cout<<"the value of a*b is :"<<a*b<<endl;
+cout<<"the value of a/b is :"<<a/b<<endl;
+}
+
+};
+class scientificcalculator{
+        
+int a,b;
+ public:
+void getdata(){
+    cout<<"enter the value of a:"<<endl;
+    cin>>a;
+    cout<<"enter the value of b:"<<endl;
+    cin>>b;
+}
+void performoperations(){
+cout<<"the value of cos(a) is :"<<cos(a)<<endl;
+cout<<"the value of sin(a) is :"<<sin(a)<<endl;
+cout<<"the value of tan(b) is :"<<tan(b)<<endl;
+cout<<"the value of exp(b) is :"<<exp(b)<<endl;
+}
+};
 int main(){
-    
+    simplecalculator c1;
+    c1.getdata();
+    c1.performoperations();
+    scientific c2;
+    c2.getdata();
+    c2.performoperations();
     return 0;
 }
