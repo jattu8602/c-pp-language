de92d41ed4ab82e6329e616436a9012018051797
jatin chourasiya <chaurasiyajatin68@gmail.com>
2024-01-04 18:10:25 +0530

    hehe

--- code stack 41-50/tut47.cpp
+++ code stack 41-50/tut47.cpp
@@ -8,13 +8,13 @@ class simplecalculator{
 
 int a,b;
 public:
-void getdata(){
+void getdatasimple(){
     cout<<"enter the value of a:"<<endl;
     cin>>a;
     cout<<"enter the value of b:"<<endl;
     cin>>b;
 }
-void performoperations(){
+void performoperationssimple(){
 cout<<"the value of a+b is :"<<a+b<<endl;
 cout<<"the value of a-b is :"<<a-b<<endl;
 cout<<"the value of a*b is :"<<a*b<<endl;
@@ -26,25 +26,34 @@ class scientificcalculator{
         
 int a,b;
  public:
-void getdata(){
+void getdatascientific(){
     cout<<"enter the value of a:"<<endl;
     cin>>a;
     cout<<"enter the value of b:"<<endl;
     cin>>b;
 }
-void performoperations(){
+void performoperationsscientific(){
 cout<<"the value of cos(a) is :"<<cos(a)<<endl;
 cout<<"the value of sin(a) is :"<<sin(a)<<endl;
 cout<<"the value of tan(b) is :"<<tan(b)<<endl;
 cout<<"the value of exp(b) is :"<<exp(b)<<endl;
 }
 };
+
+class hybridcalculator : public simplecalculator, public scientificcalculator{
+
+};
 int main(){
-    simplecalculator c1;
-    c1.getdata();
-    c1.performoperations();
-    scientificcalculator c2;
-    c2.getdata();
-    c2.performoperations();
+    // simplecalculator c1;
+    // c1.getdata();
+    // c1.performoperations();
+    // scientificcalculator c2;
+    // c2.getdata();
+    // c2.performoperations();
+    hybridcalculator calc;
+    calc.getdatasimple();
+    calc.performoperationssimple();
+    
+
     return 0;
 }
